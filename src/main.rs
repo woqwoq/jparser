1bd7ed7135394c2bb16b117d64f0412a7443d6fa
woqwoq <cim8848@gmail.com>
2026-09-13 19:56:06 +0100

    add a skeleton data types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+use std::collections::HashMap;
+
+pub enum JsonError {
+    General { row: usize, col: usize },
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum JsonValue {
+    String(String),
+    Number(f64),
+    List(Vec<JsonValue>),
+    Object(HashMap<String, JsonValue>),
+    Null,
+}
+
+pub enum Token {
+    Space,
+    Tab,
+    Char,
+    Coma,
+    CurlyBracketOpen,
+    CurlyBracketClose,
+    SquareBracketOpen,
+    SquareBracketClose,
+    Number,
+    Minus,
+    Plus,
+    Period,
+    E,
+}
+
+pub struct Lexer {
+    row: usize,
+    col: usize,
+
+    input: String,
+    structure: Vec<JsonValue>,
+}
+
+impl Lexer {
+    pub fn new(input: &str) -> Self {
+        Lexer {
+            row: 0,
+            col: 0,
+            input: input.to_string(),
+            structure: Vec::new(),
+        }
+    }
+
+    pub fn tokenize() -> Result<(), JsonError> {
+        Ok(())
+    }
+}
 fn main() {
     println!("Hello, world!");
 }
