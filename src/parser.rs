86491b648a69e81bff2a2feddeb56eceae86cd2a
woqwoq <cim8848@gmail.com>
2026-09-18 10:30:00 +0100

    add a working parse_array template

--- src/parser.rs
+++ src/parser.rs
@@ -11,6 +11,9 @@ pub struct Parser {
     cursor: usize,
 }
 
+// TODO:
+// 1. Add expect() for handling commas
+// 2. Add parse_value() for handling regular values
 impl Parser {
     pub fn new(tokens: Vec<PositionalToken>) -> Self {
         Parser { tokens, cursor: 0 }
@@ -29,7 +32,62 @@ impl Parser {
     }
 
     pub fn parse_array(&mut self) -> Result<JsonValue, SyntaxError> {
-        todo!()
+        let mut array: Vec<JsonValue> = Vec::new();
+
+        // First token has to be Token::LeftBracket [ or we error
+        if let Some(token) = self.peek()
+            && token.token == Token::LeftBracket
+        {
+            self.advance();
+        } else {
+            return Err(SyntaxError::IncompleteToken(Position::new()));
+        }
+
+        while let Some(token) = self.peek() {
+            match token.token {
+                // Seeing [ means that we are facing a nested array, so we go in recursively
+                Token::LeftBracket => {
+                    array.push(self.parse_array()?);
+                }
+                // Right bracket here means that we are at the end of the current array, so we
+                // return the current array representation (vector)
+                Token::RightBracket => {
+                    self.advance();
+                    return Ok(JsonValue::List(array));
+                }
+                Token::Comma => {
+                    if let Some(next) = self.peek_next()
+                        && matches!(
+                            next.token,
+                            Token::Null | Token::Bool(_) | Token::String(_) | Token::Number(_)
+                        )
+                    {
+                        self.advance();
+                    } else {
+                        return Err(SyntaxError::UnexpectedToken(token.position, '#'));
+                    }
+                }
+                Token::Null => {
+                    self.advance();
+                    array.push(JsonValue::Null);
+                }
+                Token::Bool(b) => {
+                    self.advance();
+                    array.push(JsonValue::Bool(b));
+                }
+                Token::Number(n) => {
+                    self.advance();
+                    array.push(JsonValue::Number(n));
+                }
+                Token::String(s) => {
+                    self.advance();
+                    array.push(JsonValue::String(s));
+                }
+                _ => return Err(SyntaxError::IncompleteToken(Position::new())),
+            };
+        }
+
+        Err(SyntaxError::IncompleteToken(Position::new()))
     }
 
     pub fn parse_object(&mut self) -> Result<JsonValue, SyntaxError> {
@@ -46,5 +104,54 @@ impl Parser {
         }
 
         Err(SyntaxError::IncompleteToken(Position::new()))
+    }
+}
+
+#[cfg(test)]
+mod parser_tests {
+    use crate::{
+        JsonValue,
+        lexer::{Lexer, PositionalToken},
+        parser::Parser,
+    };
+
+    fn build_pos_token_vec_from_input(input: &str) -> Vec<PositionalToken> {
+        Lexer::new(input).tokenize().unwrap()
+    }
+
+    fn build_parser_with_pos_token_vec_from_input(input: &str) -> Parser {
+        Parser::new(build_pos_token_vec_from_input(input))
+    }
+
+    #[test]
+    fn parse_array_passes_on_well_formed() {
+        assert_eq!(
+            build_parser_with_pos_token_vec_from_input("[]").parse_array(),
+            Ok(JsonValue::List(vec![]))
+        );
+        assert_eq!(
+            build_parser_with_pos_token_vec_from_input("[null]").parse_array(),
+            Ok(JsonValue::List(vec![JsonValue::Null]))
+        );
+        assert_eq!(
+            build_parser_with_pos_token_vec_from_input("[true]").parse_array(),
+            Ok(JsonValue::List(vec![JsonValue::Bool(true)]))
+        );
+        assert_eq!(
+            build_parser_with_pos_token_vec_from_input("[false]").parse_array(),
+            Ok(JsonValue::List(vec![JsonValue::Bool(false)]))
+        );
+        assert_eq!(
+            build_parser_with_pos_token_vec_from_input("[1]").parse_array(),
+            Ok(JsonValue::List(vec![JsonValue::Number(1.0)]))
+        );
+
+        assert_eq!(
+            build_parser_with_pos_token_vec_from_input("[1 2]").parse_array(),
+            Ok(JsonValue::List(vec![
+                JsonValue::Number(1.0),
+                JsonValue::Number(2.0)
+            ]))
+        );
     }
 }
